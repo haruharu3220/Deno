a0d26c45c777f485a63e6be3cf6043a45548e808
Hayato <hayato.314.nico@gmail.com>
2023-07-17 23:39:08 +0900

    Udemyのコードでは動かないので公式サイトから引用

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 //DenoではURLから直接ライブラリをインポートできる
-import { server } from "https://deno.land/std/http/server.ts";
-const s = server({ port:8000 });
-console.log("http://localhost:8000/");
-for await(const req of s){
-    req.respond({ statusbody: "HEllo World¥n"});
+//インポートしたURLは以下
+//https://deno.land/std@0.194.0/http/mod.ts?s=serve
+import { serve } from "https://deno.land/std@0.194.0/http/server.ts";
+serve((_req) => new Response("Hello, world"), { port: 3000 });
 
-}
+//deno run --allow-net server.tsでサーバを起動する
